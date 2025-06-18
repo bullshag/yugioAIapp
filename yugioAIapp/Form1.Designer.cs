2467983d0894193ee1e2110276934a268e2d8946
bullshag <78245141+bullshag@users.noreply.github.com>
2025-06-18 00:14:00 -0500

    Add card list and image display

--- yugioAIapp/Form1.Designer.cs
+++ yugioAIapp/Form1.Designer.cs
@@ -6,6 +6,8 @@
         ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox listBoxCards;
+        private System.Windows.Forms.PictureBox pictureBoxCard;
 
         /// <summary>
         ///  Clean up any resources being used.
@@ -28,10 +30,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxCards = new System.Windows.Forms.ListBox();
+            this.pictureBoxCard = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxCards
+            // 
+            this.listBoxCards.FormattingEnabled = true;
+            this.listBoxCards.ItemHeight = 15;
+            this.listBoxCards.Location = new System.Drawing.Point(12, 12);
+            this.listBoxCards.Name = "listBoxCards";
+            this.listBoxCards.Size = new System.Drawing.Size(260, 424);
+            this.listBoxCards.TabIndex = 0;
+            this.listBoxCards.SelectedIndexChanged += new System.EventHandler(this.listBoxCards_SelectedIndexChanged);
+            // 
+            // pictureBoxCard
+            // 
+            this.pictureBoxCard.Location = new System.Drawing.Point(278, 12);
+            this.pictureBoxCard.Name = "pictureBoxCard";
+            this.pictureBoxCard.Size = new System.Drawing.Size(510, 424);
+            this.pictureBoxCard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxCard.TabIndex = 1;
+            this.pictureBoxCard.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBoxCard);
+            this.Controls.Add(this.listBoxCards);
+            this.Name = "Form1";
+            this.Text = "Yugioh Cards";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCard)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
